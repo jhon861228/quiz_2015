9d6d10e0e990d41198ff4ac0b431a98aaa79fbd6
Jhon Diaz <dago861228@hotmail.com>
2015-06-15 11:14:39 -0500

    Despliegue con postgres

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,16 +1,27 @@
+var model = require('../models/model');
+
 exports.pregunta = function(req,res){
 	
-	var json_preguntas = [{'pregunta':'Quien descubrió América','oculto':'america'}
-								,{'pregunta':'Capital de Portugal','oculto':'portugal'}];
-	res.render('quizes/pregunta',{preguntas : json_preguntas});
+	var pregunta ;
+
+	model.Quiz.findAll().then(function(quiz){
+
+		res.render('quizes/pregunta',{pregunta : quiz[0].pregunta,id:quiz[0].id});
+	});
+	
+	
 }
 exports.respuesta = function(req,res){
 
-	var mensaje = 'Incorrecto';
-	if(req.query.pregunta==='portugal' && req.query.respuesta.toLowerCase() ==='lisboa'){
-	 mensaje = 'Correcto';
-	}else if(req.query.pregunta === 'america' && req.query.respuesta.toLowerCase() == 'cristobal colón'){
-	 mensaje = 'Correcto';
-	}
-	res.render('quizes/respuesta',{'respuesta':mensaje});	
+	
+	model.Quiz.findById(1)
+	.then(function(quiz){
+		if(quiz.respuesta.toLowerCase() === req.query.respuesta.toLowerCase()){
+			res.render('quizes/respuesta',{'respuesta':'Correcto'});		
+		}else{
+			res.render('quizes/respuesta',{'respuesta':'Incorrecto'});
+		}
+	});
+	
+
 }

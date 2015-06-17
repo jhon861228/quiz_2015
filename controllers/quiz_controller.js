427ba54f4262af04cbee402568eb9d26fd516e5c
Jhon Diaz <dago861228@hotmail.com>
2015-06-16 22:58:24 -0500

    Filtro de quizes

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,27 +1,65 @@
 var model = require('../models/model');
 
-exports.pregunta = function(req,res){
+
+exports.load = function(req,res,next,quizId){
 	
-	var pregunta ;
+	model.Quiz.findById(quizId).then(function(quiz){
+		if(quiz){
+			req.quiz = quiz;
+			next();
+		}else{
+			next(new Error('No existe quiz para '+quizId))
+		}
+	}).catch(function(error){next(error)});
+	
+}
 
-	model.Quiz.findAll().then(function(quiz){
+exports.index = function(req,res){
+	
+	if(req.query.search){
+		
+		var search = req.query.search;
+		
+		
+		search = '%'+search+'%';
+		search = search.replace(/[\s+]+/g,'%');
 
-		res.render('quizes/pregunta',{pregunta : quiz[0].pregunta,id:quiz[0].id});
-	});
+		console.log(search.replace(/[\s+]+/g,'%'));
+		
+
+		model.Quiz.findAll({
+			where: ["pregunta like ?",search]
+		})
+		.then(function(quizes){
+
+			res.render('quizes/index',{quizes : quizes});
+
+		}).catch(function(error){next(error)});
+
+	}else{
+		model.Quiz.findAll().then(function(quizes){
+
+			res.render('quizes/index',{quizes : quizes});
+		}).catch(function(error){next(error)});	
+	}
+
+	
+	
+	
+}
+
+exports.show = function(req,res){
+	
+	res.render('quizes/show',{quiz : req.quiz});
 	
 	
 }
 exports.respuesta = function(req,res){
 
+	if(req.quiz.respuesta.toLowerCase() == req.query.respuesta.toLowerCase()){
+		res.render('quizes/respuesta',{'respuesta':'Correcto','quizId':req.quiz.id});
+	}else{
+		res.render('quizes/respuesta',{'respuesta':'Incorrecto','quizId':req.quiz.id});
+	}
 	
-	model.Quiz.findById(1)
-	.then(function(quiz){
-		if(quiz.respuesta.toLowerCase() === req.query.respuesta.toLowerCase()){
-			res.render('quizes/respuesta',{'respuesta':'Correcto'});		
-		}else{
-			res.render('quizes/respuesta',{'respuesta':'Incorrecto'});
-		}
-	});
-	
-
 }
